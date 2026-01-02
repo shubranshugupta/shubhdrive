e61f607c85c13859c359b05d8c2cee00ca1163eb
Shubh Linux <shubranshugupta@gmail.com>
2026-01-02 23:07:02 +0530

    Enable configuration properties for authentication settings

--- auth-service/src/main/java/com/drive/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/drive/authservice/AuthServiceApplication.java
@@ -2,8 +2,12 @@ package com.drive.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.drive.authservice.properties.AuthProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthProperties.class)
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
